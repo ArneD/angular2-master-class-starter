2d7fa58397eb472ca265106e46c9e8b86a1fb856
Arne Dumarey <dumarey.arne@gmail.com>
2016-12-08 11:35:19 +0100

    Finished exercise 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header';
+import { ContactsService } from './contacts.service';
 
 @NgModule({
   declarations: [ContactsAppComponent, ContactsHeaderComponent],
   imports: [BrowserModule, CommonModule],
+  providers: [ContactsService],
   bootstrap: [ContactsAppComponent]
 })
 export class ContactsModule {
