0559c1c50ff70baa690627d80bde3847e03bdec3
Arne Dumarey <dumarey.arne@gmail.com>
2016-12-08 10:27:43 +0100

    Improved exercise 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header';
 
 @NgModule({
   declarations: [ContactsAppComponent, ContactsHeaderComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CommonModule],
   bootstrap: [ContactsAppComponent]
 })
 export class ContactsModule {
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
-import { NgFor } from '@angular/common';
 import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
