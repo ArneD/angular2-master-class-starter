5981030d6bd28b2ae65600c86b82d39484c72eb4
Arne Dumarey <dumarey.arne@gmail.com>
2016-12-08 10:17:55 +0100

    Finished exercise 3

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
+import { NgFor } from '@angular/common';
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -7,20 +9,5 @@ import { Contact } from './models/contact';
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
-  contact : Contact = {
-    id: 7,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/7.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  };
-
+  contacts = CONTACT_DATA;
 }
