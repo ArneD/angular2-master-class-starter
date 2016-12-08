7e1deeb6d643c009903f0b7b3d85b3222f986885
Arne Dumarey <dumarey.arne@gmail.com>
2016-12-08 16:31:46 +0100

    Finished exercise 9

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
@@ -8,12 +9,12 @@ import { ContactsService } from '../contacts.service';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  contacts: Array<Contact>;
+  contacts: Observable<Array<Contact>>;
 
   constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
-    this.contactsService.getContacts().subscribe(result => this.contacts = result);
+    this.contacts = this.contactsService.getContacts();
   }
 
 }
